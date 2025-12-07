7fe34c82b42cddaf92a0d2a538cd38d9e72032ac
Jayesh Bhardwaj <jayesh70599@gmail.com>
2025-12-08 02:26:41 +0530

    final commit before deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Donation-Dashboard/', // <--- ADD THIS LINE (Replace with your EXACT repo name)
+  base: './',  // <--- CHANGE THIS to exactly './'
 })
